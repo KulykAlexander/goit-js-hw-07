88130408a60f8ae3632ff98cafeab5805885e05f
KulykAlexander <kulikau@gmail.com>
2022-11-21 19:51:43 +0200

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,15 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-const gallery = document.querySelector(".gallery");
 
-// const list = document.createElement('ul');
-// list.classList.add('gallery__list');
-// gallery.append(list);
+const gallery = document.querySelector(".gallery");
 
 const galleryCards = galleryItems
   .map(
     (card) => `<div class="gallery__item">
-<a class="gallery__link" href=${card.original} target="_self">
+<a class="gallery__link" href="${card.original}" target="_self">
   <img
     class="gallery__image"
-    src=${card.preview}
-    data-source=${card.original}
+    src="${card.preview}"
+    data-source="${card.original}"
     alt=${card.description}
   />
 </a>
@@ -26,9 +22,20 @@ gallery.addEventListener("click", getBigImg);
 
 function getBigImg(e) {
   e.preventDefault();
-  console.log(e.target.nodeName);
-  if (e.target.nodeName !== "A") {
+
+  if (e.target.nodeName !== "IMG") {
     return;
   }
+  const imgUrlOriganal = e.target.dataset.source;
+  console.log(imgUrlOriganal);
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${imgUrlOriganal}" width="800" height="600">
+    <a>Close</a>
+    </div>
+`);
+
+  instance.show();
 }
-console.log(galleryItems);
+// console.log(galleryItems);
